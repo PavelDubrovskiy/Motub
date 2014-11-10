64730a7b33aa5dd9d941d55601717125028ab323
one-touch.ru <onetouch@dedi3142.your-server.de>
2014-11-10 17:09:19 +0100

    Updated 10.11.2013 19.12

--- www/js/vc/takePhoto/takePhotoController.js
+++ www/js/vc/takePhoto/takePhotoController.js
@@ -67,30 +67,7 @@ define(["app","js/vc/takePhoto/takePhotoView", "js/utils/user"], function(app, v
 		}, errorHandler);
 	}
 	function errorHandler(e) {
-	  var msg = '';
-	
-	  switch (e.code) {
-	    case FileError.QUOTA_EXCEEDED_ERR:
-	      msg = 'QUOTA_EXCEEDED_ERR';
-	      break;
-	    case FileError.NOT_FOUND_ERR:
-	      msg = 'NOT_FOUND_ERR';
-	      break;
-	    case FileError.SECURITY_ERR:
-	      msg = 'SECURITY_ERR';
-	      break;
-	    case FileError.INVALID_MODIFICATION_ERR:
-	      msg = 'INVALID_MODIFICATION_ERR';
-	      break;
-	    case FileError.INVALID_STATE_ERR:
-	      msg = 'INVALID_STATE_ERR';
-	      break;
-	    default:
-	      msg = 'Unknown Error';
-	      break;
-	  };
-	
-	  console.log('Error: ' + msg);
+		console.log(e);
 	}
 	return {
 		init: init
