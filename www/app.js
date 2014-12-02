cde81313c3d6253ec86c3f5b2424019e967c63b2
one-touch.ru <onetouch@dedi3142.your-server.de>
2014-12-02 16:21:53 +0100

    Updated 02.12.2014 18.25

--- www/app.js
+++ www/app.js
@@ -97,7 +97,7 @@ define('app', ['js/router'], function(Router) {
 		var params = {};
 		params.level = level;
 		params.order = order.id;
-		params.name = order.fileName+level;
+		params.name = order.fileName+photoNames[level];
 		options.params = params;
 		
 		var ft = new FileTransfer();
