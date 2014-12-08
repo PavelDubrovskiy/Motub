3c4497fd2429e348896ee36323f236c8d67a6d14
one-touch.ru <onetouch@dedi3142.your-server.de>
2014-12-08 15:37:29 +0100

    Updated 08.12.2014 17.41

--- www/js/vc/takePhoto/takePhotoController.js
+++ www/js/vc/takePhoto/takePhotoController.js
@@ -56,7 +56,7 @@ define(["app","js/vc/takePhoto/takePhotoView", "js/utils/user"], function(app, v
 				console.log(filesFS[i].params.programName+" == "+order.id+'_04_'+user.name+'_'+order.pointsNum);
 				if(filesFS[i].params.programName == order.id+'_04_'+user.name+'_'+order.pointsNum){
 					console.log('filesFS try to replace');
-					description=description.replace('photo','<img src="'+filesFS[i].params.path+'">');
+					description=description.replace('photo','<img src="'+filesFS[i].params.path+'" style="max-height:65%">');
 				}
 			}
 		}catch(e){}
